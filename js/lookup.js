fe75d15c5737ded829135fef275b9527931376a9
Sacamano604 <bentoussi@gmail.com>
2016-03-01 20:03:49 -0800

    Replacing JS with jQuery

--- js/lookup.js
+++ js/lookup.js
@@ -1,8 +1,8 @@
 var characterLookup = function(characterLookupName, characterLookupServer){
-	document.getElementById('error').innerHTML = "";
+	$('#error').html('');
 	//Putting the character lookup url, with API key in a variable
 	//Please don't use my API key :) They're free and easy to get
-	var characterPath = "https://us.api.battle.net/wow/character/" + characterLookupServer + "/" + characterLookupName + "?locale=en_US&apikey=n4t8curd5mfeupxugkqa599r2wx2x9wv";
+	var characterPath = 'https://us.api.battle.net/wow/character/' + characterLookupServer + '/' + characterLookupName + '?locale=en_US&apikey=n4t8curd5mfeupxugkqa599r2wx2x9wv';
 	//Because jsonp doesn't handle 404 callback errors we need to use a timeout function to handle the error.
 	//If the json doesn't return with a valid array within 3 seconds we'll display an error
 	//Start error handling by setting the success variable to true 
@@ -16,110 +16,110 @@ var characterLookup = function(characterLookupName, characterLookupServer){
 		var findClass = fetchedCharacter.class;
 		switch(findClass){
 			case 1:
-				className = "Warrior";
+				className = 'Warrior';
 			break;
 			case 2:
-				className = "Paladin";
+				className = 'Paladin';
 			break;
 			case 3:
-				className = "Hunter";
+				className = 'Hunter';
 			break;
 			case 4:
-				className = "Rogue";
+				className = 'Rogue';
 			break;
 			case 5:
-				className = "Priest";
+				className = 'Priest';
 			break;
 			case 6:
-				className = "Death Knight";
+				className = 'Death Knight';
 			break;
 			case 7:
-				className = "Shaman";
+				className = 'Shaman';
 			break;
 			case 8:
-				className = "Mage";
+				className = 'Mage';
 			break;
 			case 9:
-				className = "Warlock";
+				className = 'Warlock';
 			break;
 			case 10:
-				className = "Monk";
+				className = 'Monk';
 			break;
 			case 11:
-				className = "Druid";
+				className = 'Druid';
 			break;
 		}
 		//Using a switch to find the name of the race and not the actual number from JSON
 		var findRace = fetchedCharacter.race;
 		switch(findRace){
 			case 1:
-				raceName = "Human";
+				raceName = 'Human';
 			break;
 			case 2:
-				raceName = "Orc";
+				raceName = 'Orc';
 			break;
 			case 3:
-				raceName = "Dwarf";
+				raceName = 'Dwarf';
 			break;
 			case 4:
-				raceName = "Night Elf";
+				raceName = 'Night Elf';
 			break;
 			case 5:
-				raceName = "Undead";
+				raceName = 'Undead';
 			break;
 			case 6:
-				raceName = "Tauren";
+				raceName = 'Tauren';
 			break;
 			case 7:
-				raceName = "Gnome";
+				raceName = 'Gnome';
 			break;
 			case 8:
-				raceName = "Troll";
+				raceName = 'Troll';
 			break;
 			case 9:
-				raceName = "Goblin";
+				raceName = 'Goblin';
 			break;						
 			case 10:
-				raceName = "Blood Elf";
+				raceName = 'Blood Elf';
 			break;
 			case 11:
-				raceName = "Draenei";
+				raceName = 'Draenei';
 			break;
 			case 22:
-				raceName = "Worgen";
+				raceName = 'Worgen';
 			break;
 			case 24:
-				raceName = "Pandaren (Neutral)";
+				raceName = 'Pandaren (Neutral)';
 			break;
 			case 25:
-				raceName = "Pandaren (Alliance)";
+				raceName = 'Pandaren (Alliance)';
 			break;	
 			case 26:
-				raceName = "Pandaren (Horde)";
+				raceName = 'Pandaren (Horde)';
 			break;	
 		}
 		//Pushing the JSON into the span placeholders
-		document.getElementById("namePlaceholder").innerHTML = fetchedCharacter.name;
-		document.getElementById("serverPlaceholder").innerHTML = fetchedCharacter.realm;
-		document.getElementById("classPlaceholder").innerHTML = className;
-		document.getElementById("racePlaceholder").innerHTML = raceName;
-		document.getElementById("levelPlaceholder").innerHTML = fetchedCharacter.level;
-		document.getElementById("achievementPointsPlaceholder").innerHTML = fetchedCharacter.achievementPoints;
+		$('#namePlaceholder').html(fetchedCharacter.name);
+		$('#serverPlaceholder').html(fetchedCharacter.realm);
+		$('#classPlaceholder').html(className);
+		$('#racePlaceholder').html(raceName);
+		$('#levelPlaceholder').html(fetchedCharacter.level);
+		$('#achievementPointsPlaceholder').html(fetchedCharacter.achievementPoints);
 	
 	});
 		//if the timeout occurs after 3 seconds, display the following error.
 		setTimeout(function() {
 		    if (!success) {
-		        document.getElementById("error").innerHTML = "Error, character not found";
+		        $('#error').html('Error, character not found');
 		    } }, 3000);
 };
 //Ensuring the reset button resets everything including the error from the previous lookup
 var clearAll = function() {
-	document.getElementById("error").innerHTML = "";
-	document.getElementById("namePlaceholder").innerHTML = "";
-	document.getElementById("serverPlaceholder").innerHTML = "";
-	document.getElementById("classPlaceholder").innerHTML = "";
-	document.getElementById("racePlaceholder").innerHTML = "";
-	document.getElementById("levelPlaceholder").innerHTML = "";
-	document.getElementById("achievementPointsPlaceholder").innerHTML = "";
+	$('#error').html('');
+	$('#namePlaceholder').html('');
+	$('#serverPlaceholder').html('');
+	$('#classPlaceholder').html('');
+	$('#racePlaceholder').html('');
+	$('#levelPlaceholder').html('');
+	$('#achievementPointsPlaceholder').html('');
 };
