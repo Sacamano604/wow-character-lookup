93361c9c49d6d3f5dca88b5167a8e7d80616dd42
Sacamano604 <bentoussi@gmail.com>
2016-03-01 22:15:50 -0800

    CSS Edits

--- js/lookup.js
+++ js/lookup.js
@@ -1,3 +1,22 @@
+//Playing with code to have the ability to check for specific achievements
+$(document).ready(function(){
+	$.getJSON('https://us.api.battle.net/wow/character/thrall/Sacamanos?fields=achievements&locale=en_US&apikey=n4t8curd5mfeupxugkqa599r2wx2x9wv', function(returned){
+		var myarray = returned.achievements.achievementsCompleted;
+		console.log(myarray);
+		var test = myarray.indexOf(10044);
+		console.log(test);
+		if (test == -1) {
+			console.log('Not Ahead');
+		} else {
+			console.log('Ahead of the curve');
+		}
+	});
+});
+
+
+
+//https://us.api.battle.net/wow/character/thrall/Sacamanos?fields=achievements&locale=en_US&apikey=n4t8curd5mfeupxugkqa599r2wx2x9wv
+
 var characterLookup = function(characterLookupName, characterLookupServer){
 	$('#error').html('');
 	//Putting the character lookup url, with API key in a variable
@@ -12,41 +31,53 @@ var characterLookup = function(characterLookupName, characterLookupServer){
 	$.getJSON(characterPath, function(fetchedCharacter){
 		//if we can find the character flag success as true and proceed
 		success = true;
+		console.log(fetchedCharacter);
 		//Using a switch to find the name of the class and not the actual number from the JSON
 		var findClass = fetchedCharacter.class;
 		switch(findClass){
 			case 1:
 				className = 'Warrior';
+				classColor = '#C79C6E';
 			break;
 			case 2:
 				className = 'Paladin';
+				classColor = '#F58CBA';
 			break;
 			case 3:
 				className = 'Hunter';
+				classColor = '#ABD473';
 			break;
 			case 4:
 				className = 'Rogue';
+				classColor = '#FFF569';
 			break;
 			case 5:
 				className = 'Priest';
+				classColor = '#';
 			break;
 			case 6:
 				className = 'Death Knight';
+				classColor = '#C41F3B';
 			break;
 			case 7:
 				className = 'Shaman';
+				classColor = '#0070DE';
 			break;
 			case 8:
 				className = 'Mage';
+				classColor = '#69CCF0';
 			break;
 			case 9:
 				className = 'Warlock';
+				classColor = '#9482C9';
 			break;
 			case 10:
 				className = 'Monk';
+				classColor = '#00FF96';
 			break;
 			case 11:
 				className = 'Druid';
+				classColor = '#FF7D0A';
 			break;
 		}
 		//Using a switch to find the name of the race and not the actual number from JSON
@@ -100,6 +131,7 @@ var characterLookup = function(characterLookupName, characterLookupServer){
 		}
 		//Pushing the JSON into the span placeholders
 		$('#namePlaceholder').html(fetchedCharacter.name);
+		$('#namePlaceholder').css('color', classColor);
 		$('#serverPlaceholder').html(fetchedCharacter.realm);
 		$('#classPlaceholder').html(className);
 		$('#racePlaceholder').html(raceName);
